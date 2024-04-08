bb8838c2a5ae06860336263470dad71c6b7b175d
rainawan <rainawan02@gmail.com>
2024-04-07 19:00:50 -0700

    Create Grants page

--- src/pages/Research/Grants.jsx
+++ src/pages/Research/Grants.jsx
@@ -1,11 +1,54 @@
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  getKeyValue,
+  Link,
+} from "@nextui-org/react";
+import { rows, columns } from "../../components/Grants";
 import Text from "../../components/Text";
 
 const Grants = () => {
   return (
-    <div className="bg-white">
+    <div className="py-10 bg-white">
       <Text h2 className="font-bold text-darkblue">
         Grants
       </Text>
+      <div className="px-10 md:px-20 lg:px-40 py-10">
+        <Table isStriped className="text-left">
+          <TableHeader columns={columns}>
+            {(column) => (
+              <TableColumn key={column.key}>{column.label}</TableColumn>
+            )}
+          </TableHeader>
+          <TableBody items={rows}>
+            {(item) => (
+              <TableRow key={item.key}>
+                {(columnKey) => (
+                  <TableCell>
+                    {columnKey === "website" ? (
+                      item.website ? (
+                        <Link
+                          isExternal
+                          href={getKeyValue(item, columnKey)}
+                          showAnchorIcon
+                        />
+                      ) : (
+                        ""
+                      )
+                    ) : (
+                      getKeyValue(item, columnKey)
+                    )}
+                  </TableCell>
+                )}
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
     </div>
   );
 };
