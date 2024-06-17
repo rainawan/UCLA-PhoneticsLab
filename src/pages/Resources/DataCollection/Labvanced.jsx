7387c8dc8dcbc7ed99a80ab34a82eca8eefe2511
rainawan <rainawan02@gmail.com>
2024-06-17 14:07:29 -0700

    import photos for Databases. create separate header pages

---
+++ src/pages/Resources/DataCollection/Labvanced.jsx
@@ -0,0 +1,14 @@
+import Text from "../../../components/Text";
+
+const Labvanced = () => {
+  return (
+    <div>
+      <div className="pt-10">
+        <Text h3>Labvanced</Text>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Labvanced;
