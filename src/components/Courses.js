1521960a4b3369b529c248c8d8be26939a9b4dae
Khanh Nguyen <323.kvn@gmail.com>
2024-07-11 22:48:12 -0700

    add courses, fix font size for sub-labs

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -12,73 +12,118 @@ export const columns = [
     label: "OFFERED",
   },
   {
-    key: "occasional",
-    label: "OCCASIONALLY OFFERED",
-  },
-  {
-    key: "notes",
-    label: "NOTES",
+    key: "details",
+    label: "DETAILS",
   },
 ];
 
 export const undergradRows = [
   {
     key: "1",
-    course: "Linguistics 102",
+    course: "LING 102",
     name: "Introduction to Applied Phonetics",
     offered: "Winter",
-    occasional: "Summer",
+    details: "Sometimes offered summer",
   },
   {
     key: "2",
-    course: "Linguistics 103",
+    course: "LING 103",
     name: "Introduction to General Phonetics",
-    offered: "Quarterly",
-    occasional: "Summer",
+    offered: "Once a quarter",
+    details: "Sometimes offered summer",
   },
   {
     key: "3",
-    course: "Linguistics 104",
-    name: "Introduction to Applied Phonetics",
-    offered: "Winter*",
-    occasional: "-------",
+    course: "LING 104",
+    name: "Experimental Phonetics",
+    offered: "Once a year",
+    details: "Usually offered in winter",
   },
   {
     key: "4",
-    course: "Linguistics 103",
-    name: "Introduction to Applied Phonetics",
-    offered: "Winter",
-    occasional: "Summer",
+    course: "LING 111",
+    name: "Intonation",
+    offered: "Every other year",
+    details: "---",
+  },
+];
+
+export const otherUndergradRows = [
+  {
+    key: "1",
+    course: "Honors Collegium 16",
+    name: "Science of the Singing Voice",
+    offered: "Occasionally",
+    details: "---",
   },
 ];
 
 export const gradRows = [
   {
     key: "1",
-    course: "Linguistics 204A",
-    name: "Introduction to Applied Phonetics",
-    offered: "Winter",
-    occasional: "Summer",
+    course: "LING 204A",
+    name: "Experimental Phonetics",
+    offered: "Discontinued",
+    details: "No longer offered, take Ling 104 instead",
   },
   {
     key: "2",
-    course: "Linguistics 211",
-    name: "E",
-    offered: "Every third year",
-    occasional: "Summer",
+    course: "LING 211",
+    name: "Phonetic Theory",
+    offered: "See schedule",
+    details: "Alternates with Ling 111",
   },
   {
     key: "3",
-    course: "Linguistics 104",
-    name: "Introduction to Applied Phonetics",
-    offered: "Winter*",
-    occasional: "-------",
+    course: "LING 203",
+    name: "Phonetic Theory",
+    offered: "About every other year",
+    details: "",
   },
   {
     key: "4",
-    course: "Linguistics 103",
-    name: "Introduction to Applied Phonetics",
-    offered: "Winter",
-    occasional: "Summer",
+    course: "LING 204B",
+    name: "Speech Production",
+    offered: "Every third year",
+    details: "",
+  },
+  {
+    key: "4",
+    course: "LING 204C",
+    name: "Speech Production",
+    offered: "Every third year",
+    details: "",
+  },
+  {
+    key: "5",
+    course: "LING 239",
+    name: "Linguistic Methods Laboratory",
+    offered: "See schedule",
+    details: "Course on research methods",
+  },
+];
+
+export const otherGradRows = [
+  {
+    key: "1",
+    course: "LING 403",
+    name: "Practical Phonetics Training",
+    offered: "See schedule",
+    details:
+      "Graduate students attend sections of Linguistics 103 to prepare for Practical Phonetics test",
+  },
+  {
+    key: "2",
+    course: "LING 422",
+    name: "Practicum",
+    offered: "Every quarter",
+    details: "Spectogram reading",
+  },
+  {
+    key: "3",
+    course: "LING 260",
+    name: "Seminar",
+    offered: "Every quarter",
+    details: "Lab meeting",
   },
 ];
