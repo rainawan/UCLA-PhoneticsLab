ae5d19e0880d98bce14f3275b4bfb0bc3039c83b
Khanh Nguyen <323.kvn@gmail.com>
2024-08-19 13:40:07 -0700

    fix links to online data collection

--- src/pages/Resources/DataCollection/OnlineDataCollection.jsx
+++ src/pages/Resources/DataCollection/OnlineDataCollection.jsx
@@ -12,9 +12,9 @@ const OnlineDataCollection = () => {
         If you want to collect data online, you can use a platform such as{" "}
         <Link
           isExternal
-          href="https://linguistics.ucla.edu/graduate/prospective-students/graduate-admissions/"
+          href="https://www.labvanced.com/"
           underline="hover"
-          color="blue"
+          color="danger"
           size="lg"
         >
           Labvanced
@@ -24,7 +24,7 @@ const OnlineDataCollection = () => {
           isExternal
           href="https://www.mturk.com/"
           underline="hover"
-          color="blue"
+          color="danger"
           size="lg"
         >
           Mechanical Turk
@@ -34,17 +34,26 @@ const OnlineDataCollection = () => {
           isExternal
           href="https://www.prolific.com/?utm_source=google&utm_medium=ppc&utm_content=163002821037&utm_campaign=21340189877&utm_term=prolific&hsa_acc=4083515484&hsa_cam=21340189877&hsa_grp=163002821037&hsa_ad=700919351244&hsa_src=g&hsa_tgt=kwd-300570721843&hsa_kw=prolific&hsa_mt=e&hsa_net=adwords&hsa_ver=3&gad_source=1&gclid=Cj0KCQjws560BhCuARIsAHMqE0HI7sabmuJ3T2KKiiyKf7Rf4aup_LOz5TogpY34Hd1kHbXwFTBHmJkaAoTFEALw_wcB"
           underline="hover"
-          color="blue"
+          color="danger"
           size="lg"
         >
           Prolific
         </Link>
-        . The Phonetics Lab has a group account of Labvanced. So, if you want to
-        use Labvanced, please create an account in Labvanced, and give your user
-        id or email address to the lab director, informing that you want to use
-        the phonetics Lab's group account of Labvanced. To use other online
-        platforms (Mechanical Turk or Prolific), please create an account in
-        their respective website.
+        . The Phonetics Lab has a group account of{" "}
+        <Link
+          isExternal
+          href="https://www.labvanced.com/"
+          underline="hover"
+          color="danger"
+          size="lg"
+        >
+          Labvanced
+        </Link>
+        . So, if you want to use Labvanced, please create an account in
+        Labvanced, and give your user id or email address to the lab director,
+        informing that you want to use the phonetics Lab's group account of
+        Labvanced. To use other online platforms (Mechanical Turk or Prolific),
+        please create an account in their respective website.
       </p>
       <p>&nbsp;</p>
     </div>
