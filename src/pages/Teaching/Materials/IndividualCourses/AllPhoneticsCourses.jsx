36db22fa1d223fab4fd9faa6e7d6e763db111ff5
Khanh Nguyen <323.kvn@gmail.com>
2024-11-09 15:55:30 -0800

    restore previous links to epg.html to fix later

--- src/pages/Teaching/Materials/IndividualCourses/AllPhoneticsCourses.jsx
+++ src/pages/Teaching/Materials/IndividualCourses/AllPhoneticsCourses.jsx
@@ -69,9 +69,9 @@ const AllPhoneticsCourses = () => {
         </li>
 
         <li>
-          {/* <CustomLink href="../../../../../OldPhoneticsResources/physiology/epg.html#3D">
+          <CustomLink href="https://phonetics.linguistics.ucla.edu/facilities/physiology/epg.html#3D">
             Displaying Palatometer
-          </CustomLink>{" "} */}
+          </CustomLink>{" "}
           data in 3-D, by former undergrad Cheng Cheng Saw Tan
         </li>
       </ul>
