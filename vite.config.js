f23ce15a35ee1d510a7857b92e2128fc8fda304c
Khanh Nguyen <323.kvn@gmail.com>
2024-11-10 11:50:24 -0800

    add html to assets

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: [/\.(zip)$/i, "**/*.xls", "**/*.xlsx", "**/*.ppt"],
+  assetsInclude: [
+    /\.(zip)$/i,
+    "**/*.xls",
+    "**/*.xlsx",
+    "**/*.ppt",
+    "**/*.html",
+  ],
   // assetsInclude: ["**/*.xls"],
 });
