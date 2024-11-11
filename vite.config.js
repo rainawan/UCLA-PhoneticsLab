ebc605ec096630e18d1ffde8b320249c69fe5240
Khanh Nguyen <323.kvn@gmail.com>
2024-11-11 15:45:13 -0800

    remove html in vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: [
-    /\.(zip)$/i,
-    "**/*.xls",
-    "**/*.xlsx",
-    "**/*.ppt",
-    "**/*.html",
-  ],
+  assetsInclude: [/\.(zip)$/i, "**/*.xls", "**/*.xlsx", "**/*.ppt"],
   // assetsInclude: ["**/*.xls"],
 });
